f9c780f386d32c348f86953c47c553cc20b38c4a
mollikacomputer <mollikacomputer3@gmail.com>
2023-02-04 00:10:39 +0600

    payload added

--- src/Pages/Parent.js
+++ src/Pages/Parent.js
@@ -1,28 +1,190 @@
 import React, { useReducer } from "react";
+
 const Parent = () => {
-    const initialState = 0;
-    const reducer = (state, action)=>{
-        if(action.type=== "INCREMENT"){
-            return state + 1;
-        }else if(action.type==="DECREMENT"){
-            return state-1;
-        }
+  const initialState=0;
+  const reducer=(state, action)=>{
+    if(action.type==="INCREMENT"){
+      return state+action.payload.count;
+    }else if(action.type==="DECREMENT"){
+      return state-action.payload.count;
     }
-    const [state, dispatch] = useReducer(reducer, initialState);
+  }
+const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="grid justify-items-center ">
         <h2 className="text-2xl my-6">Parent {state} </h2>
           <div>
             <button
               className="btn btn-warning mr-2"
-              onClick={()=>dispatch({type:"DECREMENT"})}
+             onClick={()=> dispatch({type:"DECREMENT", payload:{count:5}})}
             >Decrement</button>
             <button
               className="btn btn-error"
-              onClick={()=>dispatch({type:"INCREMENT"})}
+              onClick={()=> dispatch({type:"DECREMENT", payload:{count:5}})}
             > Increase</button>
           </div>
     </div>
   );
 };
 export default Parent;
+// THIS IS ANOTHER PRACTICE
+// import React, { useReducer } from "react";
+
+// const Parent = () => {
+// const initialState = 0;
+// const reducer = (state, action) =>{
+//   if(action.type==="INCREMENT"){
+//     return state + 1;
+//   }else if(action.type==="DECREMENT"){
+//     return state - 1;
+//   }
+// }
+// const [state, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     <div className="grid justify-items-center ">
+//         <h2 className="text-2xl my-6">Parent {state} </h2>
+//           <div>
+//             <button
+//               className="btn btn-warning mr-2"
+//               onClick={()=> dispatch({type:"DECREMENT"})}
+//             >Decrement</button>
+//             <button
+//               className="btn btn-error"
+//               onClick={()=>dispatch({type:"INCREMENT"})}
+//             > Increase</button>
+//           </div>
+//     </div>
+//   );
+// };
+// export default Parent;
+
+// import React,{ useReducer } from "react";
+
+// const Parent = () => {
+// const initialState=0;
+// const reducer =(state, action)=>{
+//     if(action.type==="INCREMENT"){
+//         return state+1;
+//     }else if(action.type==="DECREMENT"){
+//         return state-1;
+//     }
+// };
+// const [state, dispatch] = useReducer(reducer, initialState)
+//   return (
+//     <div className="grid justify-items-center ">
+//         <h2 className="text-2xl my-6">Parent {state} </h2>
+//           <div>
+//             <button
+//               className="btn btn-warning mr-2"
+//               onClick={()=> dispatch({type:"DECREMENT"})}
+//             >Decrement</button>
+//             <button
+//               className="btn btn-error"
+//               onClick={()=> dispatch({type:"INCREMENT"})}
+//             > Increase</button>
+//           </div>
+//     </div>
+//   );
+// };
+// export default Parent;
+
+// import React, { useReducer } from "react";
+
+// const Parent = () => {
+// const initialState = 0;
+// const reducer = (state, action) =>{
+//     if(action.type==="DECREMENT"){
+//         return state-1;
+//     }else if(action.type=== "INCREMENT"){
+//         return state+1;
+//     }
+// }
+// const [state, dispatch]= useReducer(reducer, initialState);
+
+//   return (
+//     <div className="grid justify-items-center ">
+//         <h2 className="text-2xl my-6">Parent {state} </h2>
+//           <div>
+//             <button
+//               className="btn btn-warning mr-2"
+//               onClick={()=>dispatch({type:"DECREMENT"})}
+//             >Decrement</button>
+//             <button
+//               className="btn btn-error"
+//               onClick={()=> dispatch({type:"INCREMENT"})}
+//             > Increase</button>
+//           </div>
+//     </div>
+//   );
+// };
+// export default Parent;
+
+// import React, { useReducer } from "react";
+
+// const Parent = () => {
+
+//     const initialState=0;
+
+//     const reducer = (state, action) =>{
+
+//         if(action.type==="INCREMENT"){
+//             return state + 1;
+//         }else if(action.type==="DECREMENT"){
+//             return state - 1;
+//         }
+//     };
+
+//     const [state, dispatch] = useReducer(reducer, initialState);
+
+//   return (
+//     <div className="grid justify-items-center ">
+//         <h2 className="text-2xl my-6">Parent {state} </h2>
+//           <div>
+//             <button
+//               className="btn btn-warning mr-2"
+//               onClick={()=> dispatch({type:"DECREMENT"})}
+//             >Decrement</button>
+//             <button
+//               className="btn btn-error"
+//               onClick={()=> dispatch({type:"INCREMENT"}) }
+//             > Increase</button>
+//           </div>
+//     </div>
+//   );
+// };
+// export default Parent;
+
+// practice One step
+
+// import React, { useReducer } from "react";
+// const Parent = () => {
+
+//     const initialState = 0;
+
+//     const reducer = (state, action)=>{
+//         if(action.type=== "INCREMENT"){
+//             return state + 1;
+//         }else if(action.type==="DECREMENT"){
+//             return state-1;
+//         }
+//     };
+
+//     const [state, dispatch] = useReducer(reducer, initialState);
+
+//   return (
+//     <div className="grid justify-items-center ">
+//         <h2 className="text-2xl my-6">Parent {state} </h2>
+//           <div>
+//             <button
+//               className="btn btn-warning mr-2"
+//               onClick={()=>dispatch({type:"DECREMENT"})}
+//             >Decrement</button>
+//             <button
+//               className="btn btn-error"
+//               onClick={()=>dispatch({type:"INCREMENT"})}
+//             > Increase</button>
+//           </div>
+//     </div>
+//   );
+// };
+// export default Parent;
